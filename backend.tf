bea00b04cff8d0e12f5c15a85ef01a7dfe6d993f
Sumit <awatesumit@gmail.com>
2026-01-17 11:27:37 +0530

    backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "my-devops"
+        bucket = "my-devops-080907"
         key = "terraform/devops.tfstate"
         region = "us-west-1"
         dynamodb_table = "devops_test"
